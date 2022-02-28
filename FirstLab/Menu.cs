bc09fedcf364eb624c5b1d3359e62dac731e0fe9
flated <flated@mail.ru>
2022-02-28 21:43:30 +0300

    added Menu.cs

---
+++ FirstLab/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstLab
+{
+    public class Menu
+    {
+        public static void AskForInput()
+        {
+            Console.WriteLine("Как вы желаете заполнить дерево?");
+            Console.WriteLine("0 - С клавиатуры | 1 - Случайными значениями | 2 - Из файла");
+            Console.Write("Ваш выбор: ");
+        }
+    }
+}
