3182a62e613d6bbb949b99ae6631770bf5ef2627
flated <flated@mail.ru>
2022-03-12 18:55:38 +0300

    Fixed choices for tree manipulations

--- FirstLab/FIleWork.cs
+++ FirstLab/FIleWork.cs
@@ -1,1 +1,42 @@
-﻿
+﻿using System;
+using System.IO;
+
+namespace FirstLab
+{
+    public static class FileWork
+    {
+        public static Tree GetFromFile()
+        {
+
+            try
+            {
+                using (StreamReader stream = new StreamReader(GetPath()))
+                {
+                    try
+                    {
+                        
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Enter a valid filepath");
+            }
+        }
+        public static String GetPath()
+        {
+            Console.WriteLine("Enter path to file:");
+            string path = Console.ReadLine();
+            try
+            {
+                path = Path.GetFullPath(path);
+            }
+            catch
+            {
+                Console.WriteLine("Проверьте путь");
+                path = GetPath();
+            }
+            return path;
+        }
+    }
+}
